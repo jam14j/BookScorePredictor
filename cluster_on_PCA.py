037304003b2dc6179cd60019cefe0b9d5d12b77a
Juan Martinez <jam14j@my.fsu.edu>
2020-04-24 18:19:50 -0700

    Part 1 DONE

--- cluster_on_PCA.py
+++ cluster_on_PCA.py
@@ -1,56 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 24 11:09:52 2020
-Need to run PCA before this (havent actually imported module/data here)
-@author: Admin
-"""
-
-import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 
-# fields =['num_critic_for_reviews', 'duration','gross','facenumber_in_poster',
-#          'budget','movie_facebook_likes','imdb_score']
-# df = pd.read_csv("movie_metadata.csv", usecols=fields)
-# df.dropna(inplace=True)
-# df.reset_index(drop=True, inplace=True)
 # following tutorial:
 # https://towardsdatascience.com/k-means-clustering-with-scikit-learn-6b47a369a83c
-X = np.array([finalDf['principal component 1'],finalDf['imdb_score']]).T
-km = KMeans(n_clusters=3)
-y_km = km.fit_predict(X)
+def cluster_on_PCA(finalDf):
+    X = np.array([finalDf['principal component 1'],finalDf['principal component 2']]).T
+    k=5
+    km = KMeans(n_clusters=k)
+    y_km = km.fit_predict(X)
 
-
-# plot the 3 clusters
-fig = plt.figure()
-plt.xlabel("Principal Component 1")
-plt.ylabel("IMDB score")
-plt.scatter(
-    X[y_km == 0, 0], X[y_km == 0, 1],
-    s=50, c='lightgreen',
-    marker='s', edgecolor='black',
-    label='cluster 1'
-)
-plt.scatter(
-    X[y_km == 1, 0], X[y_km == 1, 1],
-    s=50, c='orange',
-    marker='o', edgecolor='black',
-    label='cluster 2'
-)
-plt.scatter(
-    X[y_km == 2, 0], X[y_km == 2, 1],
-    s=50, c='lightblue',
-    marker='v', edgecolor='black',
-    label='cluster 3'
-)
-#plot the centroids
-plt.scatter(
-    km.cluster_centers_[:, 0], km.cluster_centers_[:, 1],
-    s=250, marker='*',
-    c='red', edgecolor='black',
-    label='centroids'
-)
-plt.legend(scatterpoints=1)
-plt.grid()
-plt.show()
+    # plot the 5 clusters
+    fig = plt.figure()
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    colors = ['r','orange','yellowgreen', 'green', 'cyan', 'steelblue',
+              'midnightblue', 'mediumpurple','violet','black']
+    for i in range(k):
+        print(i)
+        plt.scatter(
+            X[y_km == i, 0], X[y_km == i, 1],
+            s=50, c=colors[i],
+            marker='o', edgecolor='black',
+            label='cluster '+str(i+1)
+        )
+    #plot the centroids
+    plt.scatter(
+        km.cluster_centers_[:, 0], km.cluster_centers_[:, 1],
+        s=250, marker='*',
+        c='yellow', edgecolor='black',
+        label='centroids'
+    )
+    plt.title("Kmeans clustering performed on the reduced data")
+    plt.legend(scatterpoints=1)
+    plt.grid()
+    print('show')
+    plt.show()
